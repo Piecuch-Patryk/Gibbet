320edbee6659ea17362b4d7b64d0a5ede7e0b626
Patryk Piecuch <patrykowo@gmail.com>
2020-04-14 23:02:58 +0200

    Add styles.

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         loader: 'file-loader',
         options: {
-          name: '[path][name].[ext]',
+          name: '[path][name].[ext]'
         },
       },
     ]
