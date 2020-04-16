38bf6648d0c9e1e522303e985ea920833f63a47f
Patryk Piecuch <patrykowo@gmail.com>
2020-04-16 10:00:56 +0200

    Fix toggle top layer.

--- src/js/modules/toggles.js
+++ src/js/modules/toggles.js
@@ -4,9 +4,9 @@ export function topLayer() {
     const el = assets.DOMelements.topLayer;
     if(el.classList.contains(assets.classes.isHidden)){
         el.classList.remove(assets.classes.fadeOut);
-        el.addEventListener('transitionend', () => el.classList.add(assets.classes.isHidden));
+        el.addEventListener('transitionend', () => el.classList.remove(assets.classes.isHidden));
     }else {
         el.classList.add(assets.classes.fadeOut);
-        el.addEventListener('transitionend', () => el.classList.remove(assets.classes.isHidden));
+        el.addEventListener('transitionend', () => el.classList.add(assets.classes.isHidden));
     }
 }
